625b60cd24f15744045e9b857db96e239365bcc7
zeroonebeatz <zeroonebeatz@gmail.com>
2019-01-02 21:00:41 +0300

    refactor(functions): functions naming

--- functions/temperature.c
+++ functions/temperature.c
@@ -4,20 +4,20 @@
 #define UPPER 300
 #define STEP 20
 
-double toCelsius(int fahr);
+double to_celsius(int fahr);
 
 int main()
 {
     int fahr;
 
     for (fahr = LOWER; fahr <= UPPER; fahr += STEP) {
-        printf("%3d %6.1f\n", fahr, toCelsius(fahr));
+        printf("%3d %6.1f\n", fahr, to_celsius(fahr));
     }
 
     return 0;
 }
 
-double toCelsius(int fahr)
+double to_celsius(int fahr)
 {
     return (5.0 / 9.0) * (fahr - 32);
 }
